c8c5de3b54afdf095ecaa80b55365ca1d9a8e55b
Yanzhen Huang <huangyanzhen0108@163.com>
2024-03-22 14:44:52 +0800

    :pencil: Regular Push

--- CISC3003-PracticeWS-2024/CISC3003-Practice08-2024MAR15/public/js/cisc3003-practice08.js
+++ CISC3003-PracticeWS-2024/CISC3003-Practice08-2024MAR15/public/js/cisc3003-practice08.js
@@ -1,34 +1,50 @@
-$(document).ready(function () {
-    // Required Fields.
-    let requiredFieldNames = ['title', 'description', 'medium', 'museum', 'year'];
+/**
+ * 
+ * @param {*} targetFieldNames A list of names of a fields to be toggled.
+ * @param {*} isError Specifies the target state of the fields, error or not.
+ * @returns Whether there are any required fields that's been incompleted.
+ */
+function toggleFieldsErrorUI(targetFieldNames, isError) {
+    var haveIncompleteFields = false;       // Whether exists any incompletes.
 
-    // On submit, check required fields.
-    $('form').on('submit', function (event) {
-        // Check each required input.
-        requiredFieldNames.forEach(function (fieldName) {
-            let field = $('#' + fieldName);
-            if (!field.val()) {
-                field.css('background', '#FFCDD2');
-                event.preventDefault();
-            } else {
-                field.css('background', 'white');
-            }
-        });
+    targetFieldNames.forEach(function (fieldName) {
+        let field = document.getElementById(fieldName);
+        if (!field.value && isError) {
+
+            // Toggle Error
+            field.classList.add("error");
+            haveIncompleteFields = true;
+        } else {
+            field.classList.remove("error");
+        }
     });
 
-    // Input field color is recovered when user clicks it.
-    $('input').on('focus', function (event) {
-        // Don't change button's color.
-        if ($(this).attr('class') == 'btn') {
-            return;
-        }
+    return haveIncompleteFields;
+}
+
+// Required Fields.
+let requiredFieldNames = ['title', 'description', 'medium', 'museum', 'year'];
+
+// Form
+document.querySelector('form').addEventListener('submit', function (event) {
+    // Toggle the UI.
+    var haveIncompleteFields = toggleFieldsErrorUI(requiredFieldNames, true);
+
+    // If there are any incompletes, don't submit.
+    if (haveIncompleteFields) {
         event.preventDefault();
+    }
+})
 
-        // Changes field color to default.
-        $(this).css('background', '');
-
-    })
-
-
-
+// On click field: Recover corresponding field UI.
+document.querySelectorAll('input').forEach(function (input) {
+    input.addEventListener('focus', function (event) {
+        input.classList.remove("error");
+    });
 });
+
+// Clear form: Recover all field UIs.
+document.querySelector('form').addEventListener('reset', function (event) {
+    toggleFieldsErrorUI(requiredFieldNames, false);
+});
+
